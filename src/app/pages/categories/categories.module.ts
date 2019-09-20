29ce2bdb9d68e0f1928a9b6b490716c07e72cbe9
Elisandro Mello Correa <elisandro@MacBook-Air-de-Elisandro.local>
2019-09-20 13:06:11 -0300

    Implementano Metodos base para CategoryForm

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule }               from '@angular/core';
+import { CommonModule }           from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { CategoriesRoutingModule } from './categories-routing.module';
-import { CategoryListComponent } from './category-list/category-list.component';
-import { CategoryFormComponent } from './category-form/category-form.component';
-
+import { CategoryListComponent }   from './category-list/category-list.component';
+import { CategoryFormComponent }   from './category-form/category-form.component';
 
 @NgModule({
   declarations: [CategoryListComponent, CategoryFormComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     CategoriesRoutingModule
   ]
 })
+
 export class CategoriesModule { }
