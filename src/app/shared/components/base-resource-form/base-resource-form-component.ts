b513beffd9c74557389b5a8e28c75b7b898dcd28
Elisandro Mello Correa <elisandro@MacBook-Air-de-Elisandro.local>
2019-09-24 14:25:48 -0300

    Criando a classe BaseResourceFormComponent

---
+++ src/app/shared/components/base-resource-form/base-resource-form-component.ts
@@ -0,0 +1,135 @@
+//Importar Modulos do Angular
+import { OnInit, AfterContentChecked, Injector }    from '@angular/core';
+import { FormBuilder, FormGroup, Validators }       from "@angular/forms";
+import { ActivatedRoute, Router }                   from "@angular/router";
+
+import { BaseResourceModel }                        from "../../models/base-resource-model";
+import { BaseResourceService }                      from "../../services/base-resource-service";
+
+//Importar Modulos de bibliotecas
+import { switchMap }                                from "rxjs/operators";
+import toastr from "toastr";
+
+export abstract class BaseResourceFormComponent <T extends BaseResourceModel> implements OnInit, AfterContentChecked {
+
+   currentAction       : String;
+   resourceForm        : FormGroup;
+   pageTitle           : String;
+   serverErrorMessage  : String[]  = null;
+   submittingForm      : boolean   = false;
+
+   protected route: ActivatedRoute;
+   protected router: Router;
+   protected formBuilder: FormBuilder;
+
+   constructor( 
+      protected injector: Injector,
+      public resource: T,
+      protected resourceService: BaseResourceService<T>,
+      protected jsonDataToResourceFn : (jsonData) => T
+   ) { 
+      this.route = this.injector.get(ActivatedRoute);
+      this.router = this.injector.get(Router);
+      this.formBuilder = this.injector.get(FormBuilder);
+   }
+   ngOnInit() {
+      this.setCurrentAction();
+      this.builderResourceForm();    
+      this.loadResource();
+   }
+
+   ngAfterContentChecked() {
+      this.setPageTitle();
+   }
+
+   submitForm() {
+      this.submittingForm = true;
+
+      if (this.currentAction == 'new') {
+         this.createResource();
+      }
+      else {
+         this.updateResource();
+      }
+   }
+
+  //Protecdes Methods
+   protected setCurrentAction(){
+      if (this.route.snapshot.url[0].path == "new") {
+         this.currentAction = 'new';
+      }
+      else {
+         this.currentAction = 'edit';
+      }
+   }
+  
+   protected loadResource() {
+      if (this.currentAction == 'edit') {
+      this.route.paramMap.pipe(
+         switchMap(params => this.resourceService.getById(+params.get("id")))
+      )
+      .subscribe(
+         (resource) => {
+            this.resource = resource;
+            this.resourceForm.patchValue(resource) //binds loaded resource  data to CresourceForm
+         },
+         (error) => alert('Ocorreu um erro no servidor, tente mais tarde')
+      )
+      }  
+   }
+
+   protected setPageTitle(){
+      if (this.currentAction == 'new') {
+         this.pageTitle = this.creationPageTitle();
+      }
+      else {
+         this.pageTitle = this.editionPageTitle();
+      }
+   }
+
+   protected creationPageTitle(): String {
+      return "Novo";
+   }
+
+   protected editionPageTitle(): String {
+      return "Edição";
+   }
+
+  protected createResource(){
+    const resource: T = this.jsonDataToResourceFn(this.resourceForm.value);
+    this.resourceService.create(resource).subscribe(
+      resource => this.actionsForSuccess(resource),
+      error => this.actionForError(error)
+    )
+  }
+
+  protected updateResource() {
+    const resource: T =  this.jsonDataToResourceFn(this.resourceForm.value);
+    this.resourceService.update(resource).subscribe(
+      resource => this.actionsForSuccess(resource),
+      error => this.actionForError(error)
+    )
+  }
+
+  protected actionsForSuccess(resource: T ) {
+    toastr.success("Solicitação processada com sucesso!!");
+    const baseComponentPath : String = this.route.snapshot.parent.url[0].path;
+
+    this.router.navigateByUrl("baseComponentPath", {skipLocationChange: true}).then(
+      () => this.router.navigate(["baseComponentPath", resource.id, "edit"])
+    )
+  }
+
+  protected actionForError(error) {
+    toastr.error("Ocorreu um erro ao processar a sua solicitação!!");
+    this.submittingForm = false;
+
+    if (error.status === 422)
+      this.serverErrorMessage = JSON.parse(error._body).errors;
+    else
+      this.serverErrorMessage = ["Falha na comunicação com o servidor. Por Favor tente mais tarde"]
+  }
+
+  protected abstract builderResourceForm(): void
+
+}
