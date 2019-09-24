2e6f6f371f3b6879f129abd012d478eb7fea5ffa
Elisandro Mello Correa <elisandro@MacBook-Air-de-Elisandro.local>
2019-09-23 23:23:21 -0300

    Melhorias no EntreService - Otimização métodos Create/Update

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,38 +1,35 @@
-import { Injectable, Injector } from '@angular/core';
-import { BaseResourceService } from "../../../shared/services/base-resource-service";
-import { Observable } from "rxjs";
-import { flatMap } from "rxjs/operators";
+import { Injectable, Injector }  from '@angular/core';
+import { BaseResourceService }   from "../../../shared/services/base-resource-service";
+import { Observable }            from "rxjs";
+import { flatMap, catchError }   from "rxjs/operators";
 
-import { CategoryService } from "../../categories/shared/category.service";
-import { Entry } from "./entry.model";
-
+import { CategoryService }       from "../../categories/shared/category.service";
+import { Entry }                 from "./entry.model";
 
 @Injectable({
-  providedIn: 'root'
+   providedIn: 'root'
 })
 export class EntryService extends BaseResourceService<Entry> {
-  constructor(protected injector: Injector, private categoryService: CategoryService) {
-    super("api/entries", injector, Entry.fromJson);
+   constructor(protected injector: Injector, private categoryService: CategoryService) {
+      super("api/entries", injector, Entry.fromJson);
    }
 
-  create(entry: Entry) : Observable<Entry> {
-    return this.categoryService.getById(entry.categoryId).pipe(
-      flatMap(category => {
-        entry.category = category;
-        return super.create(entry);
-      })
-    )
-  }
+   create(entry: Entry) : Observable<Entry> {
+      return this.setCategoryAndSendtoServer(entry, super.create.bind(this));
+   }
 
-  update(entry: Entry) : Observable<Entry> {
-    return this.categoryService.getById(entry.categoryId).pipe(
-      flatMap(category => {
-        entry.category = category;
-        return super.update(entry)
-      })
-    )
-  }
+   update(entry: Entry) : Observable<Entry> {
+      return this.setCategoryAndSendtoServer(entry, super.update.bind(this));
+   }
 
-  //Protected Methods
+   private setCategoryAndSendtoServer(entry: Entry, sendFn: any): Observable<Entry> {
+      return this.categoryService.getById(entry.categoryId).pipe(
+         flatMap(category => {
+            entry.category = category;
+            return sendFn(entry);
+         }),
+         catchError(this.handleError)
+      )
+   }
 
 }
