00f5f0df3f1d11c2b56a2f1bb614da9fa93ad534
Elisandro Mello Correa <elisandro@MacBook-Air-de-Elisandro.local>
2019-09-24 23:26:18 -0300

    Melhorando a classe CategoryList com BaseResourcelistCOmponent

--- src/app/pages/entries/entry-list/entry-list.component.ts
+++ src/app/pages/entries/entry-list/entry-list.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Entry } from "../shared/entry.model";
-import { EntryService } from "../shared/entry.service";
+import { Component, OnInit }  from '@angular/core';
+import { Entry }              from "../shared/entry.model";
+import { EntryService }       from "../shared/entry.service";
 
 @Component({
-  selector: 'app-entry-list',
-  templateUrl: './entry-list.component.html',
-  styleUrls: ['./entry-list.component.css']
+  selector    : 'app-entry-list',
+  templateUrl : './entry-list.component.html',
+  styleUrls   : ['./entry-list.component.css']
 })
 
 export class EntryListComponent implements OnInit {
